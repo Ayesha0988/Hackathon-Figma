f84eef07488d1318b3501b5b8ff3130307900b04
Ayesha0988 <ayeshasaeedr988@gmail.com>
2024-12-09 23:49:11 +0500

    Add files via upload

---
+++ src/app/essentials.tsx
@@ -0,0 +1,114 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Essentials = () => {
+  return (
+    <div>
+
+
+<div className='mt-20 mb-20'>
+        <span className='text-xl font-semibold px-10 lg:px-20 '>The Essentials</span>
+        </div>
+        
+        <div className='flex justify-evenly items-center mb-16 md:mb-28 flex-wrap m-20'>
+
+        <div className='w-700 h-[540px] overflow-hidden mb-14 md:my0 '>
+            <Image
+            className='hover:scale-105 duration-300'
+               
+               src={'/exclusive.png'}
+               alt=''
+               width={400}
+               height={540}/>
+            </div>
+
+            {/* =============2========== */}
+
+
+
+            <div className='w-600 h-[540px] overflow-hidden mb-14 md:my0 '>
+            <Image
+            className='hover:scale-105 duration-300'
+               
+               src={'/fr2.png'}
+               alt=''
+               width={400}
+               height={540}/>
+            </div>
+
+{/* ===============3============== */}
+
+<div className='w-600 h-[540px] overflow-hidden mb-14 md:my0 '>
+            <Image
+            className='hover:scale-105 duration-300'
+               
+               src={'/Frame2.png'}
+               alt=''
+               width={400}
+               height={540}/>
+            </div>
+
+
+        </div>
+
+
+        <div className='flex justify-evenly  flex-wrap px-36 pb-20 items-center '>
+
+            <div>
+                <span className='font-semibold  pl-3'>Icons</span> <br /> <br />
+                <ul className='ess-ul'>
+                    <li>Air Force 1</li>
+                    <li>Huarache</li>
+                    <li>Air Max 90</li>
+                    <li>Air Max 95</li>
+                </ul>
+
+
+            </div>
+
+
+
+
+
+
+
+            <div>
+                <span className='font-semibold  pl-3'>Shoes</span> <br /> <br />
+                 <ul className='ess-ul '>
+                    <li>All Shoes</li>
+                    <li>Custom Shoes</li>
+                    <li>Jordan Shoes</li>
+                    <li>Running Shoes</li>
+                </ul>
+            </div>
+
+
+
+            <div>
+            <span className='font-semibold  pl-3'>Clothing</span> <br /> <br />
+                 <ul className='ess-ul'>
+                    <li>All Clothing</li>
+                    <li>Modest Clothing</li>
+                    <li>Hoodies & Pullovers</li>
+                    <li>Shirts & Tops</li>
+                </ul>
+            </div>
+
+
+            <div>
+            <span className='font-semibold pl-3'>{`Kids'`}</span> <br /> <br />
+                 <ul className='ess-ul'>
+                    <li>Infant & Toddler Shoes</li>
+                    <li>`{`Kids'`} Shoes</li>
+                    <li>`{`Kids'`} Jordan Shoes</li>
+                    <li>`{`Kids'`} Basketball Shoes</li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Essentials
